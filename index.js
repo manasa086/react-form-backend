929a8d248cc2644a0e59491acc1ea3d4e1b29584
Manasa Somisetty <manasa.somisetty06@gmail.com>
2020-10-14 14:02:33 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,35 @@
+var PORT=process.env.PORT || 3001;
+var express = require('express');
+var mongodb=require("mongodb");
+var app=express();
+var mongoClient=mongodb.MongoClient;
+var url="mongodb://localhost:27017";
+var dbname="formdata";
+var cors = require("cors");
+
+app.use(cors())
+
+app.use(express.json());
+
+app.get("/",async function(req,res){
+    let client;
+    try
+    {
+    client= await mongoClient.connect(url);
+    let db=client.db(dbname);
+    client.close();
+    res.json({
+        message:"Hello"
+    })
+    }
+    catch(error)
+    {
+        if(client)
+            client.close();
+        console.log(error);
+      
+    }    
+
+})
+
+app.listen(3001);
